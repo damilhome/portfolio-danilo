523e4eaae82db5b14d41282e08421bcfef4e3aa0
Danilo <damilhome@gmail.com>
2024-09-23 17:16:40 -0300

    adicionando animação menu de linguas

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,28 @@
 const languageChooser = document.getElementById('page-language');
-const languageOptions = document.getElementById('languages-options');
+const languageOptionsContainer = document.getElementById('languages-options');
+const languageOptions = document.getElementById('languages-options-menu');
 const english = document.getElementById('english');
 const portuguese = document.getElementById('portuguese');
 const caret = document.querySelector('#page-language i:last-child');
 
+languageOptionsContainer.addEventListener('click', event => {
+    event.stopPropagation(); // Evita que o clique dentro de languageOptions afete o languageChooser
+});
+
 languageChooser.addEventListener('click', event => {
-    languageOptions.classList.toggle('disappear');
+    /* languageOptions.parentElement.classList.toggle('disappear-container'); */
+
+    if(languageOptions.classList.contains('first-disappear')){
+        languageOptions.classList.remove('first-disappear');
+        languageOptions.classList.add('disappear');
+    }
+    if(languageOptions.classList.contains('disappear')) {
+        languageOptions.classList.remove('disappear');
+        languageOptions.classList.add('appear');
+    } else {
+        languageOptions.classList.remove('appear');
+        languageOptions.classList.add('disappear');
+    }
     const isClosed = languageOptions.classList.contains('disappear')
 
     if(isClosed) {
@@ -19,8 +36,10 @@ languageChooser.addEventListener('click', event => {
 
 document.addEventListener('click', event => {
     const isClosed = languageOptions.classList.contains('disappear');
+    const isFirstClosed = languageOptions.classList.contains('first-disappear');
 
-    if(!isClosed && !languageChooser.contains(event.target)) {
+    if(!isClosed && !isFirstClosed && !languageChooser.contains(event.target)) {
+        languageOptions.classList.remove('appear');
         languageOptions.classList.add('disappear');
         languageChooser.classList.remove('open');
     }
